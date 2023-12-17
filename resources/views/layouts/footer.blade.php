d3ee47a259c5a1c81b624b75a5d3741503f28934
farahadistya <153285285+farahadistya@users.noreply.github.com>
2023-12-17 21:59:18 +0700

    UAS_ERP_B_Farah

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,27 @@
+    <!-- General JS Scripts -->
+    <script src="{{ url('assets/modules/jquery.min.js') }}"></script>
+    <script src="{{ url('assets/modules/popper.js') }}"></script>
+    <script src="{{ url('assets/modules/tooltip.js') }}"></script>
+    <script src="{{ url('assets/modules/bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="{{ url('assets/modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
+    <script src="{{ url('assets/modules/moment.min.js') }}"></script>
+    <script src="{{ url('assets/js/stisla.js') }}"></script>
+
+    <!-- Addon Javascript -->
+    <script src="{{ url('assets/modules/bootstrap-daterangepicker/daterangepicker.js') }}"></script>
+    @yield('addon-script')
+
+    <!-- Template JS File -->
+    <script src="{{ url('assets/js/scripts.js') }}"></script>
+    <script src="{{ url('assets/js/custom.js') }}"></script>
+
+    <script src="{{ url('js/toastr.js') }}"></script>
+
+    <footer class="main-footer">
+        <div class="footer-left">
+            ERP &copy; 2023 <div class="bullet"></div> Design By <a href="https://nauval.in/">Farah Wahyu A.P</a>
+        </div>
+        <div class="footer-right">
+            Dibuat dengan <i class="fas fa-heart text-danger"></i> oleh <a href="https://rafipriatna.id" target="_blank" rel="noopener noreferrer">Farah</a>
+        </div>
+    </footer>
